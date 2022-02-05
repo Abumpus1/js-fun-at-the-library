e998a567eb4d7c103acd67df07bde47dc4aa402f
Alex Bumpus <alexander.bumpus@yahoo.com>
2022-02-04 18:23:54 -0700

    Complete shelf.js

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,37 @@
+function shelfBook(bookTitle, bookShelves) {
+  if (bookShelves.length < 3){
+    bookShelves.unshift(bookTitle)
+  }
+}
+
+function unshelfBook(bookTitle, bookShelves) {
+  for (var i = 0; i < bookShelves.length; i++) {
+    if (bookShelves[i].title === bookTitle) {
+       bookShelves.splice(i, 1)
+    }
+  }
+}
+
+function listTitles(shelf) {
+  var titles = []
+  for (var i = 0; i < shelf.length; i++) {
+    titles.push(shelf[i].title)
+  }
+  return titles.join(", ")
+}
+
+function searchShelf(shelf, bookTitle) {
+  for (var i = 0; i < shelf.length; i++) {
+    if (shelf[i].title.includes(bookTitle) === true) {
+      return true
+    }
+  }
+  return false
+}
 
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+  shelfBook,
+  unshelfBook,
+  listTitles,
+  searchShelf
 };
