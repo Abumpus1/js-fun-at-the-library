0a651ffbae5212d8a637a010c197f7fdaef2d37d
Alex Bumpus <alexander.bumpus@yahoo.com>
2022-02-07 09:13:12 -0700

    Complete librarian test

--- src/librarian.js
+++ src/librarian.js
@@ -1,6 +1,36 @@
+//Ran through most of last half of these tests on librarion.js with mentor
+
+var {
+  checkoutBook
+} = require("./library.js")
+
+
 class Librarian {
-  constructor() {
-    
+  constructor(personName, library) {
+    this.name = personName;
+    this.library = library;
+  }
+  greetPatron(personName, isMorning) {
+    if (isMorning === true) {
+      return `Good morning, ${personName}!`
+    }
+    return `Hello, ${personName}!`
+  }
+  findBook(bookTitle) {
+    var shelfValues = Object.values(this.library.shelves)
+    for (var i = 0; i < shelfValues.length; i++) {
+      for (var j = 0; j < shelfValues[i].length; j++) {
+        if (shelfValues[i][j].title === bookTitle) {
+          // checkoutBook(this.library, bookTitle, shelfValues[i][j].genre) //change this
+          shelfValues[i].splice(j, 1);
+          return `Yes, we have ${bookTitle}`;
+        }
+      }
+    }
+    return `Sorry, we do not have ${bookTitle}`;
+  }
+  calculateLateFee(daysLate) {
+    return Math.ceil(daysLate * .25)
   }
 }
 
