3a370a3f429d9c362301494c0edf18f6c118b56e
Alex Bumpus <alexander.bumpus@yahoo.com>
2022-02-07 09:14:54 -0700

    Refactor librarian

--- src/librarian.js
+++ src/librarian.js
@@ -1,10 +1,5 @@
 //Ran through most of last half of these tests on librarion.js with mentor
 
-var {
-  checkoutBook
-} = require("./library.js")
-
-
 class Librarian {
   constructor(personName, library) {
     this.name = personName;
@@ -12,16 +7,15 @@ class Librarian {
   }
   greetPatron(personName, isMorning) {
     if (isMorning === true) {
-      return `Good morning, ${personName}!`
+      return `Good morning, ${personName}!`;
     }
-    return `Hello, ${personName}!`
+    return `Hello, ${personName}!`;
   }
   findBook(bookTitle) {
-    var shelfValues = Object.values(this.library.shelves)
+    var shelfValues = Object.values(this.library.shelves);
     for (var i = 0; i < shelfValues.length; i++) {
       for (var j = 0; j < shelfValues[i].length; j++) {
         if (shelfValues[i][j].title === bookTitle) {
-          // checkoutBook(this.library, bookTitle, shelfValues[i][j].genre) //change this
           shelfValues[i].splice(j, 1);
           return `Yes, we have ${bookTitle}`;
         }
@@ -30,7 +24,7 @@ class Librarian {
     return `Sorry, we do not have ${bookTitle}`;
   }
   calculateLateFee(daysLate) {
-    return Math.ceil(daysLate * .25)
+    return Math.ceil(daysLate * .25);
   }
 }
 
